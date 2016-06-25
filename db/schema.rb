78e4f7d74e926aa69e1d7b6a02f53c7eb3b02039
Héctor Salazar <h.salazar509@gmail.com>
2016-06-25 12:41:16 -0500

    Re-ran everything

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625165450) do
+ActiveRecord::Schema.define(version: 20160625170254) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20160625165450) do
 
   create_table "languages", force: :cascade do |t|
     t.string   "name"
-    t.string   "skill"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
