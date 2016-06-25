dedcb72cbe348dec5e7f4b7b1ae56f0779d30c0f
Héctor Salazar <h.salazar509@gmail.com>
2016-06-25 11:40:13 -0500

    Add validations to Hacker

--- app/models/hacker.rb
+++ app/models/hacker.rb
@@ -2,5 +2,8 @@ class Hacker < ActiveRecord::Base
   belongs_to :generation
   has_many :languages
 
+	validates :name, presence: true
+	validates :email, presence: true, uniqueness: true
+
 	accepts_nested_attributes_for :languages
 end
